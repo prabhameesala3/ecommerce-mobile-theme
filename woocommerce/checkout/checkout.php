5af23bc9f4dd82f0ba8a1f3c3752b2bea3b594a4
Prabhakara Rao M <147689616+prabhameesala3@users.noreply.github.com>
2025-11-17 13:07:57 +0530

    Add files via upload

---
+++ woocommerce/checkout/checkout.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div id="mobile-app-wrapper">
+
+    <?php get_template_part("template-parts/mobile-header"); ?>
+
+    <?php woocommerce_content(); ?>
+
+</div>
+
+<?php get_footer(); ?>
